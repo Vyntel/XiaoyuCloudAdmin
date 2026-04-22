323e6c7f384fbfab2c1293532bf689cbbddbbf95
xiaoyu <xiaoyuechoes@gmail.com>
2026-04-22 13:25:37 +0800

    feat: 添加 Knife4j 配置
    - 引入 knife4j-openapi3-jakarta-spring-boot-starter
    - 添加 Knife4jConfig 配置类
    - 配置 Swagger UI 和 API 文档
    - 添加 Knife4j 路由到忽略列表

---
+++ backend/xiaoyu-app/src/main/java/com/xiaoyu/config/Knife4jConfig.java
@@ -0,0 +1,35 @@
+package com.xiaoyu.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Knife4j 配置
+ */
+@Configuration
+public class Knife4jConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("小羽快速开发框架 API")
+                        .description("小羽快速开发框架 RESTful API 文档")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("Xiaoyu")
+                                .email("xiaoyu@example.com")
+                                .url("https://github.com/Vyntel/XiaoyuCloudAdmin")))
+                .schemaRequirement("Bearer", new SecurityScheme()
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")
+                        .description("JWT Token"))
+                .addSecurityItem(new SecurityRequirement().addList("Bearer"));
+    }
+}
